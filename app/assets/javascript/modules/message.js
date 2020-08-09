1f7a7441416b374babf103eb36fe25b7e498f54d
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-08-09 12:28:11 +0900

    7th_branch

--- app/assets/javascript/modules/message.js
+++ app/assets/javascript/modules/message.js
@@ -1,8 +1,9 @@
 $(function(){
+  let last_message_id = $('.chat-main__message-list--block:last').data("message-id") || 0;
   function buildHTML(message){
     if (message.image){
       let html =
-        `<div class="chat-main__message-list--block">
+        `<div class="chat-main__message-list--block" data-message-id=${message.id}>
           <div class="block-name">
             <div class="block-name__left">
               ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="chat-main__message-list--block">
+      `<div class="chat-main__message-list--block" data-message-id=${message.id}>
         <div class="block-name">
           <div class="block-name__left">
             ${message.user_name}
@@ -55,7 +56,7 @@ $(function(){
     .done(function(data){
       let html = buildHTML(data);
       $(".chat-main__message-list").append(html);
-      $(".chat-main__message-list").animate({scrollTop: $(".chat-main__message-list")[0].scrollHeight});
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       $("form")[0].reset();
       $(".send-botton").prop("disabled",false);
     })
@@ -63,4 +64,28 @@ $(function(){
       alert("メッセージ送信に失敗しました")
     });
   });
+
+//   let reloadMessages = function() {
+//     let last_message_id = $('.chat-main__message-form--block:last').data("message-id") || 0;
+//     $.ajax({
+//       url: "api/messages",
+//       type: 'get',
+//       dataType: 'json',
+//       //dataオプションでリクエストに値を含める
+//       data: {id: last_message_id}
+//     })
+//     .done(function(messages) {
+//       if (messages.length !== 0) {
+//         let insertHTML = '';
+//         $.each(messages, function(i, message) {
+//           insertHTML += buildHTML(message)
+//         });
+//         $('.chat-main__message-list').append(insertHTML);
+//         $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+//       }
+//     .fail(function() {
+//       alert('error');
+//     });
+//   };
+//   setInterval(reloadMessages, 7000);
 });
