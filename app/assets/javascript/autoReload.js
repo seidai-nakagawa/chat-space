cf4558d3b0cd8e6f76bef718cf75a10ade086a59
seidai-nakagawa <67330787+seidai-nakagawa@users.noreply.github.com>
2020-08-12 22:11:32 +0900

    chat-spaceのバグ修正２回目

--- app/assets/javascript/autoReload.js
+++ app/assets/javascript/autoReload.js
@@ -51,7 +51,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages)
       if (messages.length !== 0) {
         let insertHTML = '';
         $.each(messages, function(i, message) {
